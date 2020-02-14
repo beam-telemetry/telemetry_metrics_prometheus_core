52ba7afaf3a62ef63ecd819db97f774f99eef567
Bryan Naegele <bryannaegele@users.noreply.github.com>
2020-02-14 08:33:19 -0700

    Log and skip bad events vs raising (#15)
    * Log and skip bad events vs raising
    
    * Add handling for bad measure fun return

--- lib/core.ex
+++ lib/core.ex
@@ -100,6 +100,11 @@ defmodule TelemetryMetricsPrometheus.Core do
   It is recommended, but not required, to abide by Prometheus' best practices regarding labels -
   [Label Best Practices](https://prometheus.io/docs/practices/naming/#labels)
 
+  ### Missing or Invalid Measurements and Tags
+
+  If a measurement value is missing or non-numeric, the error is logged at the `debug` level
+  and the event is not recorded. Events with missing tags are also logged and skipped.
+
   """
 
   alias Telemetry.Metrics
