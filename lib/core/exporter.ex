a94ce8ce5a03c6377f39badf2f75d0b05c83a5c0
tobias-tress <tobias-tress@users.noreply.github.com>
2020-12-30 17:56:51 +0100

    Allow names with digits, strrip leading digits (#37)

--- lib/core/exporter.ex
+++ lib/core/exporter.ex
@@ -95,7 +95,8 @@ defmodule TelemetryMetricsPrometheus.Core.Exporter do
   defp format_name(name) do
     name
     |> Enum.join("_")
-    |> String.replace(~r/[^a-zA-Z_][^a-zA-Z0-9_]*/, "")
+    |> String.replace(~r/[^a-zA-Z0-9_]/, "")
+    |> String.replace(~r/^[^a-zA-Z]+/, "")
   end
 
   defp escape(value) do
